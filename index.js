088d6b0e9cdd2edaa002391c707e6a85fc1f1b7e
JoshBarrass99 <161977190+JoshBarrass99@users.noreply.github.com>
2024-08-15 15:46:45 +0100

    15/8/24 - fixed field validation highlight
    added field validation highlights depending on whether the value matches the regex

--- index.js
+++ index.js
@@ -1,62 +1,104 @@
 
-const validateCardNumber =number => {
-    //Check if the number contains only numeric value  
-    //and is of between 13 to 19 digits
-    const regex = new RegExp("^[0-9]{16}$");
-    if (!regex.test(number)) {
-        return false;
-    }
 
-    return luhnCheck(number);
-}
+// Name Input Highlight
 
-const luhnCheck = val => {
-    let checksum = 0; // running checksum total
-    let j = 1; // takes value of 1 or 2
+function checkName(){
+  document.getElementById('nameInput').className = ""
 
-    // Process each digit one by one starting from the last
-    for (let i = val.length - 1; i >= 0; i--) {
-      let calc = 0;
-      // Extract the next digit and multiply by 1 or 2 on alternative digits.
-      calc = Number(val.charAt(i)) * j;
+  let nameFormat = /^[A-Za-z ]+$/
 
-      // If the result is in two digits add 1 to the checksum total
-      if (calc > 9) {
-        checksum = checksum + 1;
-        calc = calc - 10;
-      }
+  let f = document.getElementById("nameInput").value
 
-      // Add the units element to the checksum total
-      checksum = checksum + calc;
-
-      // Switch the value of j
-      if (j == 1) {
-        j = 2;
-      } else {
-        j = 1;
-      }
-    }
+  if(f.match(nameFormat)){
+    // alert('make green')
+    document.getElementById('nameInput').classList.add("makeGreen")
+  }
+  else{
+    // alert('makered')
+    document.getElementById('nameInput').classList.add("makeRed")
+    alert("Please enter only alphabetical characters")
+  }
   
-    //Check if it is divisible by 10 or not.
-    return (checksum % 10) == 0;
 }
 
+// Email input highlight
 
+function checkEmailInput() {
+
+  document.getElementById("emailInput").className = ''
+
+  let emailFormat = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+  let g = document.getElementById("emailInput").value
+
+  if(g.match(emailFormat)){
+    document.getElementById('emailInput').classList.add("makeGreen")
+  }
+  else{
+    document.getElementById('emailInput').classList.add("makeRed")
+    alert("Please enter a valid email")
+  }
+}
 
 // Credit Card Input Highlight 
 
 function checkCreditField() {
 
-    document.getElementById('cardInput').className = ''
+  document.getElementById('cardInput').className = ''
 
-    let f = document.getElementById('cardInput').value
+  let h = document.getElementById('cardInput').value
 
-    if (validateCardNumber(f) == true) {
-        document.getElementById('cardInput').classList.add('makeGreen')
-    }
-    else document.getElementById("cardInput").classList.add("makeRed")
+  if (validateCardNumber(h) == true) {
+      document.getElementById('cardInput').classList.add('makeGreen')
+  }
+  else document.getElementById("cardInput").classList.add("makeRed")
+  alert("Please enter a valid 16-digit card number")
 }
-document.getElementById("cardInput").addEventListener(oninput, checkCreditField(f))
 
-// Name Input Highlight
+//LUHN algorithm 
+
+const validateCardNumber =number => {
+  //Check if the number contains only numeric value  
+  //and is of between 13 to 19 digits
+  const regex = new RegExp("^[0-9]{16}$");
+  if (!regex.test(number)) {
+      return false;
+  }
+
+  return luhnCheck(number);
+}
+
+const luhnCheck = val => {
+  let checksum = 0; // running checksum total
+  let j = 1; // takes value of 1 or 2
+
+  // Process each digit one by one starting from the last
+  for (let i = val.length - 1; i >= 0; i--) {
+    let calc = 0;
+    // Extract the next digit and multiply by 1 or 2 on alternative digits.
+    calc = Number(val.charAt(i)) * j;
+
+    // If the result is in two digits add 1 to the checksum total
+    if (calc > 9) {
+      checksum = checksum + 1;
+      calc = calc - 10;
+    }
+
+    // Add the units element to the checksum total
+    checksum = checksum + calc;
+
+    // Switch the value of j
+    if (j == 1) {
+      j = 2;
+    } else {
+      j = 1;
+    }
+  }
+
+  //Check if it is divisible by 10 or not.
+  return (checksum % 10) == 0;
+}
+
+
+
 
